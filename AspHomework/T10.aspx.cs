c883cd599760bd2f8ab3fcf605aefd5c503686d8
Doreamonsky <403036847@qq.com>
2019-11-18 19:35:18 +0800

    Add T10

---
+++ AspHomework/T10.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class T10 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.Append("<p>" + Environment.MachineName + "</p>");
+        stringBuilder.Append("<p>" + System.Web.HttpUtility.UrlEncode(Request.PhysicalPath) + "</p>");
+        Response.Write(Server.HtmlEncode(stringBuilder.ToString()));
+        Response.Write(stringBuilder.ToString());
+        Response.End();
+
+    }
+
+}
